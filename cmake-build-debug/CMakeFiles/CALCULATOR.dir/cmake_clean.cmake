726ced585f3689a0fc99a183de85f82c9a65d6f6
eriolnik <lozarik16@yandex.ru>
2021-01-19 16:12:49 +0300

    1
    1

---
+++ cmake-build-debug/CMakeFiles/CALCULATOR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CALCULATOR"
+  "CALCULATOR.pdb"
+  "CMakeFiles/CALCULATOR.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CALCULATOR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, result = 0;
+char arifm;
+
+void console() {
+    cout << endl << a << arifm << b << "=" << result << endl << "------------------------------" << endl;
+}
+
+int main() {
+    while (1) {
+        cout << "Ivan Alexsandrovich, please, enter your expression: " << endl;
+        cin >> a >> arifm >> b;
+        switch (arifm) {
+            case '+':
+                result = a + b;
+                console();
+                break;
+            case '-':
+                result = a - b;
+                console();
+                break;
+            case '*':
+                result = a * b;
+                console();
+                break;
+            case '/':
+                result = a / b;
+                console();
+                break;
+            default:
+                cout << "Incorrect expression, please, try again!" << endl << "------------------------------" << endl;
+                break;
+        }
+    }
+    return 0;
+}
 
